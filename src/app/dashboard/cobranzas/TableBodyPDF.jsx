84d78e785dec32517cebaf91cab49d52613e8c94
ramirochangomoreno <rama.exe.13@gmail.com>
2023-07-14 14:05:17 -0300

    filtranso conceptos en el body de la tabla

--- src/app/dashboard/cobranzas/TableBodyPDF.jsx
+++ src/app/dashboard/cobranzas/TableBodyPDF.jsx
@@ -14,23 +14,28 @@ const tw = createTw({
 });
 
 export default function TableBodyPDF({ array, label, style }) {
-
-
-    const NOPINTARCAMPOS=["conceptos","obsercacionesPagoRealizado","cicloLectivo","uidPago"]
-  return   array?.map((fila,i)=>(
-    <View key={i}
+  const NOPINTARCAMPOS = [
+    "concepto",
+    "obsercacionesPagoRealizado",
+    "cicloLectivo","hora",
+    "uidPago",
+  ];
+  return array?.map((fila, i) => (
+    <View
+      key={i}
       style={tw(
         " flex items-center justify-between odd:bg-orange-100/50 flex-row w-full py-2 px-2 mx-auto flex-grow border-y"
       )}
     >
       {Object.entries(fila)
-  .filter(([key]) => !NOPINTARCAMPOS.includes(key))
-  .map(([key, value], i) => (
-    <View key={i} style={style}>
-      <Text>{value}</Text>
+        .filter(([key]) => !NOPINTARCAMPOS.includes(key))
+        .map(([key, value], i) => (
+          <View key={i} style={tw(
+            " text-gray-700 font-medium py-1"
+          )}>
+            <Text>{value}</Text>
+          </View>
+        ))}
     </View>
-  ))
-      }
-    </View>
-
-))}
+  ));
+}
